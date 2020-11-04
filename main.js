e09f7c9c3336b9416d39c9b0801ea7d64c9bec78
杜审言 <2645472474@qq.com>
2020-11-04 16:12:24 +0800

    背景图自动切换

---
+++ main.js
@@ -0,0 +1,9 @@
+var imgs = ["./imges/背景图片.jpg", "./imges/背景图片1.jpg"];
+var i = 0;
+var body = document.getElementById("budai");
+body.style.backgroundImage = "url(./imges/背景图片.jpg)";
+setInterval(() => {
+  i++;
+  i = i % 2;
+  body.style.backgroundImage = "url(" + imgs[i] + ")"
+}, 3000)
